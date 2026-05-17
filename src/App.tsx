d925dff18180a74b7fd30b63ec9d904b0b5597f8
Igor Minayeu <igor.minayeu@gmail.com>
2026-05-17 12:55:28 +0300

    add routes for component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne.tsx";
+import {PageTwo} from "./components/pages/PageTwo.tsx";
+import {PageThree} from "./components/pages/PageThree.tsx";
+import {Route, Routes} from "react-router-dom";
 
-export const App = ()=> {
-  return (
-    <div>
-      <div className={styles.header}><h1>HEADER</h1></div>
-      <div className={styles.body}>
-        <div className={styles.nav}>
-          Здесь будет навигация
+export const App = () => {
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    Здесь будет навигация
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path='/page1' element={<PageOne/>}/>
+                        <Route path='/page2' element={<PageTwo/>}/>
+                        <Route path='/page3' element={<PageThree/>}/>
+                    </Routes>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
-        <div className={styles.content}>
-          Здесь будут кроссовки
-        </div>
-      </div>
-      <div className={styles.footer}>abibas 2023</div>
-    </div>
-  );
+    );
 }
