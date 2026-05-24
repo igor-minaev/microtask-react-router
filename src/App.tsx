2fc6934b0aab48b467aae494a035a864ae26bfeb
Igor Minayeu <igor.minayeu@gmail.com>
2026-05-24 14:50:56 +0300

    start lesson 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import styles from "./components/Site.module.css";
 import {PageOne} from "./components/pages/PageOne.tsx";
 import {PageTwo} from "./components/pages/PageTwo.tsx";
 import {PageThree} from "./components/pages/PageThree.tsx";
-import {Navigate, Route, Routes} from "react-router-dom";
+import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404.tsx";
+
 
 export const App = () => {
     return (
@@ -11,7 +12,8 @@ export const App = () => {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                   <div><NavLink to='/page1'>Page1</NavLink></div>
+
                 </div>
                 <div className={styles.content}>
                     <Routes>
@@ -21,8 +23,8 @@ export const App = () => {
                         <Route path='/page2' element={<PageTwo/>}/>
                         <Route path='/page3' element={<PageThree/>}/>
 
-                        <Route path='/error404' element={<Error404/>}/>
-                        <Route path='/*' element={<Navigate to='/error404'/>}/>
+                        <Route path='/page/error' element={<Error404/>}/>
+                        <Route path='/*' element={<Navigate to='/page/error'/>}/>
                     </Routes>
                 </div>
             </div>
